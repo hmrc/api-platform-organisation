6c0d9b008ae47f6b751d09ff2c770c3189b3e2f0
Anjum Abbas <49648217+anjumabbas5@users.noreply.github.com>
2025-02-26 13:20:02 +0000

    APIS-7501 - Add companies house key to run local script (#15)

--- run_local.sh
+++ run_local.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sbt "~run -Drun.mode=Dev -Dhttp.port=15504 -Dapplication.router=testOnlyDoNotUseInAppConf.Routes $*"
+sbt "~run -Drun.mode=Dev -Dhttp.port=15504 -Dapplication.router=testOnlyDoNotUseInAppConf.Routes -Dcompanies-house.api-key=${COMPANIES_HOUSE_KEY} $*"
