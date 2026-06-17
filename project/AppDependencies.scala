78418982258f4690184e56ae3a4fa7d43e1752cb
kskourtis1 <kskourtis1@users.noreply.github.com>
2026-06-17 10:04:10 +0100

    APIS-8171: api-platform-organisation - Address Bobby violations (#49)
    * APIS-8171: api-platform-organisation - Address Bobby violations
    
    * APIS-8171: api-platform-organisation - Address Bobby violations - bump lib version

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,15 +4,14 @@ object AppDependencies {
 
   private val bootstrapVersion = "10.7.0"
   private val hmrcMongoVersion = "2.12.0"
-  private val orgDomainVersion = "0.19.0"
+  private val orgDomainVersion = "0.20.0"
   private val tpdDomainVersion = "0.15.0"
 
   val compile = Seq(
     "uk.gov.hmrc"       %% "bootstrap-backend-play-30"        % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"               % hmrcMongoVersion,
     "uk.gov.hmrc"       %% "api-platform-organisation-domain" % orgDomainVersion,
-    "uk.gov.hmrc"       %% "api-platform-tpd-domain"          % tpdDomainVersion,
-    "commons-validator"  % "commons-validator"                % "1.7"
+    "uk.gov.hmrc"       %% "api-platform-tpd-domain"          % tpdDomainVersion
   )
 
   val test = Seq(
