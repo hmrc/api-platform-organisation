679774f1951e34765bd11fc818159362919a23aa
Toby Gilham <623674+Tobias1087@users.noreply.github.com>
2026-09-03 10:49:52 +0100

    APIS-8311 - Tidy up to use implicit hc and explicitly set headers

--- app/uk/gov/hmrc/apiplatformorganisation/connectors/OrganisationsMatchingApiConnector.scala
+++ app/uk/gov/hmrc/apiplatformorganisation/connectors/OrganisationsMatchingApiConnector.scala
@@ -35,11 +35,7 @@ import uk.gov.hmrc.apiplatformorganisation.models.SaMatchingRequest
 class OrganisationsMatchingApiConnector @Inject() (http: HttpClientV2, config: AppConfig)(implicit val ec: ExecutionContext) extends Logging {
   lazy val serviceBaseUrl: String = config.ucrCustomerApiUri
 
-  def matchOrganisationSa(request: SaMatchingRequest, hc: HeaderCarrier): Future[JsValue] = {
-    implicit val headerCarrier: HeaderCarrier = hc.copy(
-//      authorization = Some(Authorization(s"Basic ${config.ucrToken}")),
-//      extraHeaders = Seq("CorrelationId" -> UUID.randomUUID().toString, "system-id" -> "APIPlatform")
-    )
+  def matchOrganisationSa(request: SaMatchingRequest)(implicit hc: HeaderCarrier): Future[JsValue] = {
 
     http.post(url"$serviceBaseUrl/mulesoft/customer/v2/api/organisations/identifier-search")
       .withBody(Json.toJson(request))
